8f4f6f1cc2ac3828c244401e6574da271f999d25
TuAnhTuan <letuanhtuan7@gmail.com>
2024-12-25 00:34:05 +0700

    feat: Modify layout sendEmail OTP

--- banking/src/main/java/com/wnc/banking/service/EmailService.java
+++ banking/src/main/java/com/wnc/banking/service/EmailService.java
@@ -40,13 +40,23 @@ public class EmailService {
     }
 
     public String getOtpLoginEmailTemplate(String name, String accountNumber, String otp) {
-        return "<div>"
-                + "<h3>Hi, " + name + "</h3>"
-                + "<p>Account Number: " + accountNumber + "</p>"
-                + "<p>Your OTP is:</p>"
-                + "<h2>" + otp + "</h2>"
-                + "<p>Regards,</p>"
-                + "<p>DomLandBank Team</p>"
+        String emailTemplate = "<div style=\"font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2\">"
+                + "<div style=\"margin:50px auto;width:70%;padding:20px 0\">"
+                + "<div style=\"border-bottom:1px solid #eee\">"
+                + "<a href=\"https://domlandbank.netlify.app/\" style=\"font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600\">DomLandBank</a>"
+                + "</div>"
+                + "<p style=\"font-size:1.1em\">Hi, " + name + "</p>"
+                + "<p style=\"font-size:0.9em;\">Account Number: " + accountNumber + "</p>"
+                + "<p>Thank you for choosing DomLandBank. Use the following OTP to complete your Log In procedures. OTP is valid for 5 minutes</p>"
+                + "<h2 style=\"background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;\">" + otp + "</h2>"
+                + "<p style=\"font-size:0.9em;\">Regards,<br />DomLandBank</p>"
+                + "<hr style=\"border:none;border-top:1px solid #eee\" />"
+                + "<p>Group 1 Inc</p>"
+                + "<p>Anh_Huu_Loc_Phat_Tuan</p>"
+                + "<p>Khoa hoc Tu nhien - HCMUS</p>"
+                + "</div>"
                 + "</div>";
+
+        return emailTemplate;
     }
 }
