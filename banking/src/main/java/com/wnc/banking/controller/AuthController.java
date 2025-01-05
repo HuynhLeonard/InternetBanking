176a5cf6eb6295d100da7cc7640cd7bc6958954a
Lucas-VTL <votanloc1106@gmail.com>
2025-01-05 18:56:02 +0700

    Complete api doc for auth controller

--- banking/src/main/java/com/wnc/banking/controller/AuthController.java
+++ banking/src/main/java/com/wnc/banking/controller/AuthController.java
@@ -5,6 +5,12 @@ import com.wnc.banking.dto.AuthResponse;
 import com.wnc.banking.dto.LoginRequest;
 import com.wnc.banking.dto.RefreshTokenRequest;
 import com.wnc.banking.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
@@ -18,12 +24,75 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Authentication", description = "Endpoints for user authentication and token management")
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(
+            summary = "Login to the application",
+            description = "Authenticate the user and generate access and refresh tokens if the credentials are valid."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Login successfully",
+                    content = @Content(mediaType = "application/json", examples = @ExampleObject(
+                            name = "Login successfully",
+                            value = "{\n" +
+                                    "  \"success\": true,\n" +
+                                    "  \"message\": \"Successfully logged in\",\n" +
+                                    "  \"data\": \"AuthResponse{accessToken='string', refreshToken='string', message='string'}\"\n" +
+                                    "}"))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Cannot found user via email",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Cannot found user with email: email@123\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Invalid password",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Invalid password\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Missing email",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Email is required\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Missing password",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Password is required\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Invalid email address",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Invalid email address\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}")
+                            })),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Internal server error",
+                                    value = "{\n" +
+                                            "  \"success\": false,\n" +
+                                            "  \"message\": \"Internal server error message\",\n" +
+                                            "  \"data\": \"null\"\n" +
+                                            "}")))
+    })
     @PostMapping("/login")
     ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody @Valid LoginRequest request, BindingResult result) {
         if (result.hasErrors()) {
@@ -46,6 +115,62 @@ public class AuthController {
         }
     }
 
+    @Operation(
+            summary = "Refresh access token",
+            description = "Refresh the access token using a valid refresh token."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "Refreshing access token successfully",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Refresh successfully",
+                                    value = "{\n" +
+                                            "  \"success\": true,\n" +
+                                            "  \"message\": \"Successfully refresh access token\",\n" +
+                                            "  \"data\": \"AuthResponse{accessToken='string', refreshToken='string', message='string'}\"\n" +
+                                            "}"))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(mediaType = "application/json",
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Invalid refresh token",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Invalid refresh token\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Expired refresh token",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Refresh token expired\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Cannot found user via refresh token",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Cannot found user with id: user123\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}"),
+                                    @ExampleObject(
+                                            name = "Missing refresh token",
+                                            value = "{\n" +
+                                                    "  \"success\": false,\n" +
+                                                    "  \"message\": \"Refresh token is required\",\n" +
+                                                    "  \"data\": \"null\"\n" +
+                                                    "}")
+                    })),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "Internal Server Error",
+                    content = @Content(mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Internal server error",
+                                    value = "{\n" +
+                                        "  \"success\": false,\n" +
+                                        "  \"message\": \"Internal server error message\",\n" +
+                                        "  \"data\": \"null\"\n" +
+                                        "}")))
+    })
     @PostMapping("/refresh")
     ResponseEntity<ApiResponse<AuthResponse>> refreshAccessToken(@RequestBody @Valid RefreshTokenRequest request, BindingResult result) {
         if (result.hasErrors()) {
